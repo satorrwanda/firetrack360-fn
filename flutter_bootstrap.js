caad04a6ef5d434ecc5d325cee1056f02b764433
MugemaneBertin2001 <MugemaneBertin2001@users.noreply.github.com>
2025-01-28 12:16:44 +0000

    deploy: 15e132774a9818548fff72bc61729b2f43d7a760

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "900080826"
+    serviceWorkerVersion: "2511737119"
   }
 });
